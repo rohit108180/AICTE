ec83193de61bc64a11f94d401b11bfb290b55b83
Amogh_Garg <amoghgarg9@gmail.com>
2023-09-18 20:08:13 +0530

    Fixed editor page buttons

--- client/src/Documents/Main.js
+++ client/src/Documents/Main.js
@@ -1,107 +1,101 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Popup from "./Popup";
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
 
 export default function Main() {
+  const { state } = useLocation();
+  const { _id, Title, Desc, User } = state.Repo;
+  const [Version, setVersion] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [Content, setContent] = useState(
+    "<h3> This Repository is Currently Empty ... </h3>"
+  );
+  const navigate = useNavigate();
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleCloseX = () => {
+    setIsOpen(false);
+  };
 
-const {state} = useLocation();
-const {_id, Title, Desc, User} = state.Repo;
-const [Version,setVersion] = useState([])
-const [isOpen, setIsOpen] = useState(false);
-const [Content, setContent] = useState("<h3> This Repository is Currently Empty ... </h3>");
-const navigate=useNavigate()
-const togglePopup = () => {
-  setIsOpen(!isOpen);
-};
-const handleCloseX =()=>{
-  setIsOpen(false)
-}
+  useEffect(() => {
+    getData();
+  }, []);
 
-useEffect(()=>{
-  getData()
-},[])
-
-const getData=async()=>{
-console.log(_id)
-  const response = await axios.get(`http://localhost:5000/version/all/${state.Repo._id}`)
-  setVersion(response.data)
-  if(response.data.length>0){
-  getContent(response.data)}
-}
-const getContent=async(array)=>{
-    const obj = array[array.length - 1] 
-    const response = await axios.get(`http://localhost:5000/version/${obj._id}`)
-    console.log(response.data)
-    setContent(response.data.Content)
-  }
-  const Edit =()=>{ 
-    state.Repo.Content = Content
-    navigate(`/Edit/${state.Repo.Title}`, { state: { Repo:state.Repo } })    
-  }  
-const getCon = async(id)=>{
-    const response = await axios.get(`http://localhost:5000/version/${id}`)
-    setContent(response.data.Content)
-  
-} 
-  return(
-    <div className="container" style={{ height: 700, width: "100%", padding: 20 }}>
-
-    <div className="col d-flex justify-content-end">
-    <button
-      class="btn btn-primary ms-auto me-0 me-md-3 my-2 my-md-0"
-      type="button"
-      id="cource-btn"
-      onClick={togglePopup}
+  const getData = async () => {
+    console.log(_id);
+    const response = await axios.get(
+      `http://localhost:5000/version/all/${state.Repo._id}`
+    );
+    setVersion(response.data);
+    if (response.data.length > 0) {
+      getContent(response.data);
+    }
+  };
+  const getContent = async (array) => {
+    const obj = array[array.length - 1];
+    const response = await axios.get(
+      `http://localhost:5000/version/${obj._id}`
+    );
+    console.log(response.data);
+    setContent(response.data.Content);
+  };
+  const Edit = () => {
+    state.Repo.Content = Content;
+    navigate(`/Edit/${state.Repo.Title}`, { state: { Repo: state.Repo } });
+  };
+  const getCon = async (id) => {
+    const response = await axios.get(`http://localhost:5000/version/${id}`);
+    setContent(response.data.Content);
+  };
+  return (
+    <div
+      className="container"
+      style={{ height: 700, width: "100%", padding: 20 }}
     >
-        Versions
-    </button>
-    <button
-      class="btn btn-primary "
-      style={{width:70}}
-      type="button"
-      id="cource-btn"
-      onClick={Edit}
-    >
-        Edit
-    </button>
+      <div className="col d-flex justify-content-end">
+        <Button variant="contained" onClick={togglePopup}>
+          Versions
+        </Button>
+        <Button variant="outlined" onClick={Edit}>
+          Edit
+        </Button>
+      </div>
+
+      {isOpen && (
+        <Popup
+          content={
+            <>
+              <h4 style={{ textAlign: "center", marginBottom: 30 }}>
+                Versionitory Detail
+              </h4>
+              <table class="table" id="list">
+                <thead>
+                  <tr>
+                    <th scope="col">Version</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Version.map((Repo, index) => {
+                    return (
+                      <tr scope="row" key={Repo._id}>
+                        <td onClick={() => getCon(Repo._id)}>
+                          Version: {index + 1}
+                        </td>
+                        <td>{Repo.Desc}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </>
+          }
+          handleClose={handleCloseX}
+        />
+      )}
+      <div dangerouslySetInnerHTML={{ __html: Content }}></div>
     </div>
-
-    {isOpen && (
-            <Popup
-              content={
-                <>
-                  <h4 style={{ textAlign: "center", marginBottom: 30 }}>
-                    Versionitory Detail
-                  </h4>
-                  <table class="table" id="list">
-        <thead>
-          <tr>
-            <th scope="col">Version</th>
-          </tr>
-        </thead>
-        <tbody>
-
-        {Version.map((Repo,index) => {
-              return (
-                <tr scope="row" key={Repo._id}> 
-                  <td onClick={()=>getCon(Repo._id)}>Version: {index+1}</td>
-                  <td>{Repo.Desc}</td>
-                </tr>
-              );
-            })}      
-          </tbody>
-      </table>
-                </>
-              }
-              handleClose={handleCloseX}
-            />
-          )}
-          <div dangerouslySetInnerHTML={{__html: Content}}>
-        
-        </div>
-    </div>
-    )
-
+  );
 }
-
