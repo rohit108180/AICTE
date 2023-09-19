4dba21c7b9febf4ee87b338844737d7aa23d45ed
rohit108180 <rohit108180@gmail.com>
2023-09-20 01:12:04 +0530

    cors origin changed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // allow all origins
 // app.use(cors());
-app.use(cors({origin:'https://aicte-portal-frontend.onrender.com/',credentials:true}));
+app.use(cors({origin:'https://aicte-portal-frontend.onrender.com',credentials:true}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
