02833fc47ee652ce621989543a66638877edf81a
Amogh_Garg <amoghgarg9@gmail.com>
2023-09-19 19:17:57 +0530

    Analytics page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,72 @@
 import {
   BrowserRouter as Router,
   Routes,
-  Route,Navigate
-} from "react-router-dom"
-import ProtectedRoute from "./ProtectedRoute"
-import Navigation from "./Navigation"
-import Login from "./Login Signup/Login"
-import Register from "./Login Signup/Register"
-import Repositories from "./Repositories/Repositories"
-import TextEditor from "./Documents/TextEditor"
-import Main from "./Documents/Main"
+  Route,
+  Navigate,
+} from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import Navigation from "./Navigation";
+import Login from "./Login Signup/Login";
+import Register from "./Login Signup/Register";
+import Repositories from "./Repositories/Repositories";
+import TextEditor from "./Documents/TextEditor";
+import Main from "./Documents/Main";
 import { AuthProvider } from "./Context/AuthProvider";
 
-import { v4 as uuidV4 } from "uuid"
-import { Profile } from "./pages/Profile/Profile"
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Landing } from "./pages/LandingPage/Landing"
-import Resources from "./Resources/Resources"
-import { AppProvider } from "./Context/context/appContext"
-import SingIn from "./Login Signup/SingIn"
-import SignUp from "./Login Signup/SignUp"
+import { v4 as uuidV4 } from "uuid";
+import { Profile } from "./pages/Profile/Profile";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Landing } from "./pages/LandingPage/Landing";
+import Resources from "./Resources/Resources";
+import { Analytics } from "./Analytics/Analytics.js";
+import { AppProvider } from "./Context/context/appContext";
+import SingIn from "./Login Signup/SingIn";
+import SignUp from "./Login Signup/SignUp";
 
 function App() {
-
-
   // src/theme.js
 
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1E7C83', // Your primary color
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#1E7C83", // Your primary color
+      },
+      secondary: {
+        main: "#DFECED", // Your secondary color
+      },
     },
-    secondary: {
-      main: '#DFECED', // Your secondary color
+    typography: {
+      fontFamily: "Arial, sans-serif", // Your preferred font family
     },
-  },
-  typography: {
-    fontFamily: 'Arial, sans-serif', // Your preferred font family
-  },
-});
-
-
-
-
+  });
 
   return (
     <Router>
       <ThemeProvider theme={theme}>
-      <AuthProvider>
-      <AppProvider>
-       <Routes>
-       <Route element={<Navigation />}>
-        <Route path="/"element={<Landing />}/> 
-        <Route path="/login" element={<SingIn />}/>
-        <Route path="/Register" element={<SignUp/>}/>
-        
-        <Route element={<ProtectedRoute />}>
-          <Route path="/Dashboard" element={<Repositories/>}/>
-          <Route path="/Profile" element={<Profile/>}/>
-          <Route path="/Resources" element={<Resources/>}/>
-          <Route path="/:Title" element={<Main />}/>
-          <Route path="/Edit/:Title" element={<TextEditor />}/>
-        </Route>
-        </Route>
-        <Route path="*"element={<Navigate to="/" />}/>
-       </Routes>
-       </AppProvider>
-       </AuthProvider>
+        <AuthProvider>
+          <AppProvider>
+            <Routes>
+              <Route element={<Navigation />}>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<SingIn />} />
+                <Route path="/Register" element={<SignUp />} />
 
-       </ThemeProvider>
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/Dashboard" element={<Repositories />} />
+                  <Route path="/Profile" element={<Profile />} />
+                  <Route path="/Resources" element={<Resources />} />
+                  <Route path="/Analytics" element={<Analytics />} />
+                  <Route path="/:Title" element={<Main />} />
+                  <Route path="/Edit/:Title" element={<TextEditor />} />
+                </Route>
+              </Route>
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </AppProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
